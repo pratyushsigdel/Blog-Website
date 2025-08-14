e7b26161924e6c8b3c0705e262553c53b4733a14
ron <pratyushsigdel9@gmail.com>
2025-08-14 16:51:50 +0545

    feat: integrate Tailwind CSS and set up authentication with Appwrite
    - Added Tailwind CSS and its Vite plugin to the project.
    - Removed unused CSS files and replaced global styles with Tailwind's utility classes.
    - Implemented authentication service using Appwrite, including methods for login, logout, and user management.
    - Created Redux slice for authentication state management.
    - Set up basic Header and Footer components for layout.
    - Configured environment variables for Appwrite integration.
    - Updated main application structure to include Redux Provider for state management.

---
+++ src/appwrite/auth.js
@@ -0,0 +1,73 @@
+import conf from "../conf/conf";
+
+import { Client, Account, ID } from "appwrite";
+
+//Creating class first to perform authenication service from App Write
+export class AuthService {
+  client = new Client();
+  account;
+
+  //We have access to constructor from the object that we created below
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  //Method to create Account for a user
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      // Checking whether userAccount Exist
+      if (userAccount) {
+        //Call Login method Directly
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("AppWrite Service Error:: getCurrent User", error);
+    }
+  }
+  //Creating Login Method
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("AppWrite Service Error:: Login User", error);
+    }
+  }
+
+  //Checking Authentication State Of User
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("AppWrite Service Error:: getCurrent User", error);
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite Service Error", error);
+    }
+  }
+}
+
+//Creating object from AuthService class and exporting it
+
+const authService = new AuthService();
+
+export default authService;
